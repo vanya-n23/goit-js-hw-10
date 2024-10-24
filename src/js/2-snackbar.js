675a48f802bf53dcd2c0eb5a811ad7d3a66c09f0
vanya-n23 <vanya.neresnitsia@gmail.com>
2024-10-24 20:37:16 +0300

    5

---
+++ src/js/2-snackbar.js
@@ -0,0 +1,43 @@
+
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit);
+
+function onSubmit(event) {
+  event.preventDefault();
+
+  
+  const formData = new FormData(form);
+  const delay = parseInt(formData.get('delay')); 
+  const state = formData.get('state'); 
+
+  
+  createPromise(delay, state)
+    .then(delay => {
+      iziToast.success({
+        title: 'Success',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+      });
+    })
+    .catch(delay => {
+      iziToast.error({
+        title: 'Error',
+        message: `❌ Rejected promise in ${delay}ms`,
+      });
+    });
+}
+
+function createPromise(delay, state) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+}
